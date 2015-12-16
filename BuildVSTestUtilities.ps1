44f038ca229109b586045fce5744ddc63a2cc591
omerr <ravi3O4M>
2015-12-16 17:15:20 +0200

    Added NuGet packages and build PowerShell script

---
+++ BuildVSTestUtilities.ps1
@@ -0,0 +1,14 @@
+﻿$vsVersions = @{"14.0" = "2015"; "12.0" = "2013";  "11.0"= "2012"}
+
+foreach($ver in $vsVersions.GetEnumerator())
+{
+    $versionNumber = $ver.Key
+    $versionName = $ver.Value
+    msbuild VSTestUtilities.sln /P:VSTarget="$versionNumber" /P:Configuration=Release /P:Platform=x86
+    VSTestUtilities\Build\nuget pack VSTestUtilities\VSTestUtilities.nuspec `
+					/Prop VSTarget="$versionNumber" `
+					/Prop VSVersion="$versionName" `
+					/OutputDirectory VSTestUtilities\BuildOutput
+}
+
+
